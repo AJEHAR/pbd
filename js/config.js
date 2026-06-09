839ca67ac1d4e8e2b8cec34f59e0ba3831858590
AJEHAR <g-39150004@moe-dl.edu.my>
2026-06-09 16:11:40 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,9 @@ const CONFIG = {
   // Paste URL Apps Script selepas deploy sebagai Web App
   // Pergi ke Apps Script → Deploy → New Deployment → Web App
   // Execute as: Me | Who has access: Anyone
-  API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzDcT3QaCDJTbjGqXty3rwX5HWZBVACvcCQpdfu-33UjOxAM1qK4Pb_LEOlYSDHsqnk/exec',
 
   SCHOOL_NAME_BM : 'Sekolah Kebangsaan Pendidikan Khas Kuantan',
-  SCHOOL_NAME_EN : 'Kuantan Special Education National School',
+  SCHOOL_NAME_EN : 'Sekolah Kebangsaan Pendidikan Khas Kuantan',
   SCHOOL_LOGO    : 'https://i.postimg.cc/9Fc5bWCH/LOGO-SEKOLAH-TIADA-BG.png',
 };
